4001af875ff5e1b6b90248ece0ba42510e644d4b
adsonb87 <adsonb87@gmail.com>
2023-07-18 20:56:19 -0300

    firebase

--- backend/config.ts
+++ backend/config.ts
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAeVnxgjvkUoGS3kKTjFdx8lTQ25FVt80U",
-  authDomain: "next-crud-19e73.firebaseapp.com",
-  projectId: "next-crud-19e73",
-  storageBucket: "next-crud-19e73.appspot.com",
-  messagingSenderId: "122298294536",
-  appId: "1:122298294536:web:3b5336fd9e618438904cbd"
+  apiKey: "AIzaSyD_CxApZ6x2I3mU3zPBHPeziLfuo-aW4KM",
+  authDomain: "next-crud-6d4ac.firebaseapp.com",
+  projectId: "next-crud-6d4ac",
+  storageBucket: "next-crud-6d4ac.appspot.com",
+  messagingSenderId: "81882475688",
+  appId: "1:81882475688:web:1e0c9002e1809970f1669d"
 };
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
-
-console.log("Firebase" + firebase)
 
 export default firebase
